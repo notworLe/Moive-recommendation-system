cb6ccf16d4841276bfdf24da2cee9779ad9f39fd
Lê Đức Huy <duchuy.notworle@gmail.com>
2026-01-16 16:12:59 +0700

    Hello alpha branch

--- recommendation.py
+++ recommendation.py
@@ -6,3 +6,5 @@ data = pd.read_csv('data/movies.csv', encoding='latin-1', sep='\t')
 
 data = data[['title', 'genres']]
 data['genres'] = data['genres'].apply(lambda x : x.replace('|', ' '))
+
+print('Hello alpha branch')
